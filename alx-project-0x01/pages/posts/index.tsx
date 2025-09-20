e5d553e8747f01886ca8563b535e89171157954c
Favour Idowu <favouridowu02@gmail.com>
2025-09-20 20:11:09 +0100

    feat: create post interface

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,13 +1,46 @@
-import Header from "@/components/layout/Header"
+import Header from "@/components/layout/Header";
+import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
+import React from "react";
+import { title } from "process";
 
-export default function Posts() {
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+    console.log(posts)
     return (
-        <>
+        <div className="flex flex-col h-screen">
             <Header/>
-            <div className="container mt-4">
-                <h2>Posts</h2>
-                <p>This is the posts page.</p>
-            </div>
-        </>
+            <main className="p-4">
+                <div className="flex justify-between">
+                    <h1 className="text-2xl font-semibold">
+                        Post Content
+                    </h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+                        Add Post
+                    </button>
+                </div>
+                <div className="grid grid-cols-3 gap-2">
+
+                    {
+                        posts?.map(({title, body, userId, id}: PostProps, key: number) => (
+                            <PostCard title={title} body={body} userId={userId} id={id} key={key}/>
+                        ))
+                    }
+                </div>
+            </main>
+        </div>
     )
 }
+
+
+export async function getStaticProps() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json()
+
+    return {
+        props: {
+            posts
+        }
+    }
+}
+
+export default Posts;
