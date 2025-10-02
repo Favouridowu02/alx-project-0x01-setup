6f82476b9183f518659ee7a55a8c0bd859a69fd7
Favour Idowu <favouridowu02@gmail.com>
2025-10-02 18:51:02 +0100

    feat: implement the users page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,28 +1,47 @@
-import Header from "@/components/layout/Header"
-import UserCard from "@/components/common/UserCard"
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces"
 import React from "react"
 
 
-const Users: React.FC<UserProps[]> = ({ users }) => {
+const Users: React.FC<{users: UserProps[]}> = ( {users} ) => {
     console.log(users)
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <Header/>
+            <main className="p-4">
+                <div className="flex justify-between">
+                    <h1 className="text-2xl font-semibold">
+                        User Content
+                    </h1>
+                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+                        Add User
+                    </button>
+                </div>
+                <div className="grid grid-cols-3 gap-2">
+                    {
+                        users?.map(({id, name, username, email, address, phone, website, company}: UserProps, key: number) => (
+                            <UserCard key={key} id={id} name={name} username={username} email={email} address={address} phone={phone} website={website} company={company} />
+                        ))
+                    }
+
+                </div>
+            </main>
+
         </div>
 
     )
 }
 
 export async function getStaticProps() {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users")
-    const users = response.json();
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
 
     return {
         props: {
             users
         }
-    }
+    };
 }
 
 export default Users;
